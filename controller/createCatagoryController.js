59b4138d81e16169a43ddc178c08a17ceeed018a
Sourov-HossainDev <sourovhossain2326@gmail.com>
2024-03-18 01:41:22 +0600

    catagory controller done

--- controller/createCatagoryController.js
+++ controller/createCatagoryController.js
@@ -1,6 +1,18 @@
+const CatagoryList = require("../models/catagorySchema");
 
-function createCatagoryController(){
-    console.log('ji  careate catagory');
+async function createCatagoryController(req, res) {
+    const { name, description } = req.body;
+    // res.send(name);
+    const duplicateCatagory = await CatagoryList.find({name});
+    if(duplicateCatagory.length > 0){
+        res.json({error: 'This catagory already exist, try again'})
+    }
+    const category = new CatagoryList({
+        name,
+        description
+    })
+    res.json({success: 'catagorye create successfully'})
+    category.save();
 }
 
-module.exports = createCatagoryController;
+module.exports = createCatagoryController;   
