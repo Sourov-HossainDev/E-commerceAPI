30440dc6526eec512b41026698b15c6611ed1240
Sourov-HossainDev <sourovhossain2326@gmail.com>
2024-03-17 23:37:43 +0600

    create a subCatagory

---
+++ models/catagorySchema.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const catagorySchema = new Schema({
+    name : {
+        type : String,
+        required: true
+    },
+    description: String,
+
+    isActive : {
+        type : Boolean,
+        default: false
+    },
+    status : {
+        type : String,
+        default: 'waiting',
+        enum: ['waiting', 'rejected', 'approved'] 
+    },
+    created: {
+        type: Date,
+        default : new Date()
+    },
+    update: {
+        type : Date
+    }
+
+})
+
+module.exports = mongoose.model('CatagoryList', catagorySchema)
