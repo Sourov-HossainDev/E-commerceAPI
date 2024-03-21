fa63f2ca9332dca88e34b235ddb44bd6cb1565e7
Sourov-HossainDev <sourovhossain2326@gmail.com>
2024-03-21 23:38:53 +0600

    working product portion

---
+++ controller/productController.js
@@ -0,0 +1,6 @@
+
+function productController(){
+
+}
+
+module.exports = productController;
--- route/api/index.js
+++ route/api/index.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const authRouter = require('./authentication')
 const catagoryRouter = require('./catagory')
 const marchantRouter = require('./marchant')
+const productRouter = require('./product')
 
 const authapiRoute = process.env.BASE_AUTH_URL
 router.use(authapiRoute, authRouter);
 router.use('/catagory', catagoryRouter )
 router.use('/marchant', marchantRouter )
+router.use('/product', productRouter )
 
 
 module.exports = router;
